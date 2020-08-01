d1c306e2cd2e8df094502404a9129d7d82631f28
Krushang Kasundra <kruzkasu223@gmail.com>
2020-08-01 15:54:50 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,239 @@
+
+const songName01 = document.querySelector(".song_01");
+/* const songName02 = document.querySelector(".song_02");
+const songName03 = document.querySelector(".song_03");
+const songName04 = document.querySelector(".song_04");
+const songName05 = document.querySelector(".song_05");
+const songName06 = document.querySelector(".song_06");
+const songName07 = document.querySelector(".song_07");
+const songName08 = document.querySelector(".song_08"); */
+const songName09 = document.querySelector(".song_09");
+/* const songName10 = document.querySelector(".song_10");
+
+const songName11 = document.querySelector(".song_11");
+const songName12 = document.querySelector(".song_12");
+const songName13 = document.querySelector(".song_13");
+const songName14 = document.querySelector(".song_14");
+const songName15 = document.querySelector(".song_15");
+const songName16 = document.querySelector(".song_16");
+const songName17 = document.querySelector(".song_17");
+const songName18 = document.querySelector(".song_18");
+const songName19 = document.querySelector(".song_19");
+const songName20 = document.querySelector(".song_20");
+
+const songName21 = document.querySelector(".song_21");
+const songName22 = document.querySelector(".song_22");
+const songName23 = document.querySelector(".song_23");
+const songName24 = document.querySelector(".song_24");
+const songName25 = document.querySelector(".song_25");
+const songName26 = document.querySelector(".song_26");
+const songName27 = document.querySelector(".song_27");
+const songName28 = document.querySelector(".song_28");
+const songName29 = document.querySelector(".song_29");
+const songName30 = document.querySelector(".song_30");
+
+const songName31 = document.querySelector(".song_31");
+const songName32 = document.querySelector(".song_32");
+const songName33 = document.querySelector(".song_33");
+const songName34 = document.querySelector(".song_34");
+const songName35 = document.querySelector(".song_35");
+const songName36 = document.querySelector(".song_36");
+const songName37 = document.querySelector(".song_37");
+const songName38 = document.querySelector(".song_38");
+const songName39 = document.querySelector(".song_39");
+const songName40 = document.querySelector(".song_40");
+
+const songName41 = document.querySelector(".song_41");
+const songName42 = document.querySelector(".song_42");
+const songName43 = document.querySelector(".song_43");
+ */
+
+var song01 = new Audio("song_01.mp3");
+/* var song02 = new Audio("song_02.mp3");
+var song03 = new Audio("song_03.mp3");
+var song04 = new Audio("song_04.mp3");
+var song05 = new Audio("song_05.mp3");
+var song06 = new Audio("song_06.mp3");
+var song07 = new Audio("song_07.mp3");
+var song08 = new Audio("song_08.mp3"); */
+var song09 = new Audio("song_09.flac");
+/* var song10 = new Audio("song_10.mp3");
+
+var song11 = new Audio("song_11.mp3");
+var song12 = new Audio("song_12.mp3");
+var song13 = new Audio("song_13.mp3");
+var song14 = new Audio("song_14.mp3");
+var song15 = new Audio("song_15.mp3");
+var song16 = new Audio("song_16.mp3");
+var song17 = new Audio("song_17.mp3");
+var song18 = new Audio("song_18.mp3");
+var song19 = new Audio("song_19.mp3");
+var song20 = new Audio("song_20.mp3");
+
+var song21 = new Audio("song_21.mp3");
+var song22 = new Audio("song_22.mp3");
+var song23 = new Audio("song_23.mp3");
+var song24 = new Audio("song_24.mp3");
+var song25 = new Audio("song_25.mp3");
+var song26 = new Audio("song_26.mp3");
+var song27 = new Audio("song_27.mp3");
+var song28 = new Audio("song_28.mp3");
+var song29 = new Audio("song_29.mp3");
+var song30 = new Audio("song_30.mp3");
+
+var song31 = new Audio("song_31.mp3");
+var song32 = new Audio("song_32.mp3");
+var song33 = new Audio("song_33.mp3");
+var song34 = new Audio("song_34.mp3");
+var song35 = new Audio("song_35.mp3");
+var song36 = new Audio("song_36.mp3");
+var song37 = new Audio("song_37.mp3");
+var song38 = new Audio("song_38.mp3");
+var song39 = new Audio("song_39.mp3");
+var song40 = new Audio("song_40.mp3");
+
+var song41 = new Audio("song_41.mp3");
+var song42 = new Audio("song_42.mp3");
+var song43 = new Audio("song_43.mp3");
+ */
+
+
+songName01.addEventListener("click", () => {
+    song09.pause();
+    song01.play();
+});/* 
+songName02.addEventListener("click", () => {
+    song02.play();
+});
+songName03.addEventListener("click", () => {
+    song03.play();
+});
+songName04.addEventListener("click", () => {
+    song04.play();
+});
+songName05.addEventListener("click", () => {
+    song05.play();
+});
+songName06.addEventListener("click", () => {
+    song06.play();
+});
+songName07.addEventListener("click", () => {
+    song07.play();
+});
+songName08.addEventListener("click", () => {
+    song08.play();
+}); */
+songName09.addEventListener("click", () => {
+    song01.pause();
+    song09.play();
+});/* 
+songName10.addEventListener("click", () => {
+    song10.play();
+});
+
+
+songName11.addEventListener("click", () => {
+    song11.play();
+});
+songName12.addEventListener("click", () => {
+    song12.play();
+});
+songName13.addEventListener("click", () => {
+    song13.play();
+});
+songName14.addEventListener("click", () => {
+    song14.play();
+});
+songName15.addEventListener("click", () => {
+    song15.play();
+});
+songName16.addEventListener("click", () => {
+    song16.play();
+});
+songName17.addEventListener("click", () => {
+    song17.play();
+});
+songName18.addEventListener("click", () => {
+    song18.play();
+});
+songName19.addEventListener("click", () => {
+    song19.play();
+});
+songName20.addEventListener("click", () => {
+    song20.play();
+});
+
+
+songName21.addEventListener("click", () => {
+    song21.play();
+});
+songName22.addEventListener("click", () => {
+    song22.play();
+});
+songName23.addEventListener("click", () => {
+    song23.play();
+});
+songName24.addEventListener("click", () => {
+    song24.play();
+});
+songName25.addEventListener("click", () => {
+    song25.play();
+});
+songName26.addEventListener("click", () => {
+    song26.play();
+});
+songName27.addEventListener("click", () => {
+    song27.play();
+});
+songName28.addEventListener("click", () => {
+    song28.play();
+});
+songName29.addEventListener("click", () => {
+    song29.play();
+});
+songName30.addEventListener("click", () => {
+    song30.play();
+});
+
+
+songName31.addEventListener("click", () => {
+    song31.play();
+});
+songName32.addEventListener("click", () => {
+    song32.play();
+});
+songName33.addEventListener("click", () => {
+    song33.play();
+});
+songName34.addEventListener("click", () => {
+    song34.play();
+});
+songName35.addEventListener("click", () => {
+    song35.play();
+});
+songName36.addEventListener("click", () => {
+    song36.play();
+});
+songName37.addEventListener("click", () => {
+    song37.play();
+});
+songName38.addEventListener("click", () => {
+    song38.play();
+});
+songName39.addEventListener("click", () => {
+    song39.play();
+});
+songName40.addEventListener("click", () => {
+    song40.play();
+});
+
+
+songName41.addEventListener("click", () => {
+    song41.play();
+});
+songName42.addEventListener("click", () => {
+    song42.play();
+});
+songName43.addEventListener("click", () => {
+    song43.play();
+}); */
